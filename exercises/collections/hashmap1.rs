f1174d89fe5485e72704fb15ff365e58933f006a
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-21 19:31:41 -0300

    new challange completed - hashmap1

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,34 +11,34 @@
 // Execute the command `rustlings hint hashmap1` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+  let mut basket = HashMap::new();
 
-    // Two bananas are already given for you :)
-    basket.insert(String::from("banana"), 2);
+  // Two bananas are already given for you :)
+  basket.insert(String::from("banana"), 2);
+  basket.insert(String::from("apple"), 3);
+  basket.insert(String::from("pear"), 4);
 
-    // TODO: Put more fruits in your basket here.
+  // TODO: Put more fruits in your basket here.
 
-    basket
+  basket
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn at_least_three_types_of_fruits() {
-        let basket = fruit_basket();
-        assert!(basket.len() >= 3);
-    }
+  #[test]
+  fn at_least_three_types_of_fruits() {
+    let basket = fruit_basket();
+    assert!(basket.len() >= 3);
+  }
 
-    #[test]
-    fn at_least_five_fruits() {
-        let basket = fruit_basket();
-        assert!(basket.values().sum::<u32>() >= 5);
-    }
+  #[test]
+  fn at_least_five_fruits() {
+    let basket = fruit_basket();
+    assert!(basket.values().sum::<u32>() >= 5);
+  }
 }
