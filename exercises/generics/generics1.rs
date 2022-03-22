84dbf1f4bb95d1d37885d254068aa54f9dc537cf
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-22 18:47:48 -0300

    new challange completed - generics1

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -6,6 +6,6 @@
 // I AM NOT DONE
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
-    shopping_list.push("milk");
+  let mut shopping_list: Vec<&str> = Vec::new();
+  shopping_list.push("milk");
 }
