28ac5324598ab8e4bf548e540d95c25a81e52658
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-25 15:40:07 -0300

    new challange completed - traits1

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -8,36 +8,38 @@
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
-
 trait AppendBar {
-    fn append_bar(self) -> Self;
+  fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    //Add your code here
+  fn append_bar(mut self) -> Self {
+    self.push_str("Bar");
+
+    self
+  }
 }
 
 fn main() {
-    let s = String::from("Foo");
-    let s = s.append_bar();
-    println!("s: {}", s);
+  let s = String::from("Foo");
+  let s = s.append_bar();
+  println!("s: {}", s);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn is_FooBar() {
-        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
-    }
+  #[test]
+  fn is_FooBar() {
+    assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
+  }
 
-    #[test]
-    fn is_BarBar() {
-        assert_eq!(
-            String::from("").append_bar().append_bar(),
-            String::from("BarBar")
-        );
-    }
+  #[test]
+  fn is_BarBar() {
+    assert_eq!(
+      String::from("").append_bar().append_bar(),
+      String::from("BarBar")
+    );
+  }
 }
