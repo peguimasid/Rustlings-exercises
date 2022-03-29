7a30a4b295556d183e58cfd97e026814803a74cc
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-29 14:34:06 -0300

    new challenge completed - box1

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,41 +16,43 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
-    Nil,
+  Link(i32, Box<List>),
+  Nil,
 }
 
 fn main() {
-    println!("This is an empty cons list: {:?}", create_empty_list());
-    println!(
-        "This is a non-empty cons list: {:?}",
-        create_non_empty_list()
-    );
+  println!("This is an empty cons list: {:?}", create_empty_list());
+  println!(
+    "This is a non-empty cons list: {:?}",
+    create_non_empty_list()
+  );
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+  let result = List::Nil;
+  result
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+  List::Link(
+    1,
+    Box::new(List::Link(2, Box::new(List::Link(3, Box::new(List::Nil))))),
+  )
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_create_empty_list() {
-        assert_eq!(List::Nil, create_empty_list())
-    }
+  #[test]
+  fn test_create_empty_list() {
+    assert_eq!(List::Nil, create_empty_list())
+  }
 
-    #[test]
-    fn test_create_non_empty_list() {
-        assert_ne!(create_empty_list(), create_non_empty_list())
-    }
+  #[test]
+  fn test_create_non_empty_list() {
+    assert_ne!(create_empty_list(), create_non_empty_list())
+  }
 }
